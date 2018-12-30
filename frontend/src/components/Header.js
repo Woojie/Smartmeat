4dc217e8fa0311c31901343f40a4795e29a905fc
karyma101 <karyma101@hotmail.com>
2018-12-29 21:20:52 -0800

    fixed navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -14,10 +14,12 @@ const Header = ( {logout, loggedIn} ) => {
     </button></Link>
     return (
       <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-white">
-        <a className="navbar-brand" href="/">
-          <img src={logo} alt="logo" width="20"/>
-          SmartMeat
-        </a>
+        <span className="navbar-brand">
+          <Link to="/">
+            <img src={logo} alt="logo" width="20"/>
+            SmartMeat
+          </Link>
+        </span>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -25,16 +27,18 @@ const Header = ( {logout, loggedIn} ) => {
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="/ourTool">Our Tool</a>
-            </li>
-            <li className="nav-item">
-              <Link to="community">
-                <p className="nav-link" href="community">Community</p>
+              <Link to="/ourTool">
+                <span className="nav-link">Our Tool</span>
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="profile">
-                <p className="nav-link">Profile</p>
+              <Link to="/community">
+                <span className="nav-link">Community</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/profile">
+                <span className="nav-link">Profile</span>
               </Link>
             </li>
           </ul>
