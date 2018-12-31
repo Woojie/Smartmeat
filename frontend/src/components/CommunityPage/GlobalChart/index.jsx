0d36a6ade74312966ce74e6d48f024f4786069d4
Wooj <wjchung@hotmail.ca>
2018-12-31 13:57:23 -0800

    added a bit of info

--- frontend/src/components/CommunityPage/GlobalChart/index.jsx
+++ frontend/src/components/CommunityPage/GlobalChart/index.jsx
@@ -37,13 +37,18 @@ class GlobalChart extends React.Component {
 
     })
   
-    
+    let directHousehold = Math.round((alteredCarbon+userCarbonDitched) / 41)
+
     
     
     return(
       <div>
       <h1>Global Community</h1>
-      <h4>Total: {carbon + userCarbon}kg of greenhouse gas emissions created by SmartMeat users.</h4>
+      <h5>Total: {carbon + userCarbon}kg of greenhouse gas emissions created by SmartMeat users.</h5>
+      <h5>{userCarbonDitched + alteredCarbon}kg of greenhouse gas emissions saved by SmartMeat Users!!</h5>
+      <span>With the amount saved, you can heat, cool and drive an average Canadian household for <b>{directHousehold}days!</b> </span>
+    <img src='./images/house.svg' alt="house" />
+      <br />
       <br />
       <Breakup 
         email={email}
