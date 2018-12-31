dfd97b71090ebba16603703334e0e883936ee4f3
Kary Ma <karyma101@hotmail.com>
2018-12-30 20:31:49 -0800

    added team section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,13 +5,18 @@ import communityIcon from '../resources/icon/community.svg'
 import awarenessIcon from '../resources/icon/awareness.svg'
 import calculateIcon from '../resources/icon/calculate.svg'
 
+import munifa from '../resources/profile/munifa_ahmad.jpg'
+import wooj from '../resources/profile/wooj_chung.jpg'
+import roman from '../resources/profile/roman_iudin.jpg'
+import kary from '../resources/profile/kary_ma.jpg'
+
 const Home = () => {
   return (
     <div>
-      <header id="showcase">
-        <div id="showcase-container" className="d-flex flex-column justify-content-center align-items-center">
-          <h3 id="showcase-title" className="bg-light">Less Meat, Less Heat, More Life...</h3>
-          <video id="showcase-video" className="rounded" src={video} controls></video>
+      <header className="showcase">
+        <div className="showcase-container d-flex flex-column justify-content-center align-items-center">
+          <h3 className="showcase-title bg-light">Less Meat, Less Heat, More Life...</h3>
+          <video className="showcase-video rounded" src={video} controls></video>
         </div>
       </header>
 
@@ -31,7 +36,7 @@ const Home = () => {
         </div>
       </section>
 
-      <section className="bg-light py-5">
+      <section className="light py-5">
         <div className="mx-5">
           <div className="row">
             <div className="col-12 col-md-4">
@@ -50,6 +55,42 @@ const Home = () => {
               <img className="d-block mx-auto" src={communityIcon} alt="community icon" height="100px "/>
               <h3 className="text-center">Join our community</h3>
               <p>Coming together to reduce our CO2 emmisions by building a community of green thinkers.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <header class="showcase" style={{height: '20vh'}}></header>
+
+      <section className="py-5">
+        <div className="mx-5">
+          <div className="row">
+            <h3 className="text-center col-12 mb-5">Our Team</h3>
+          </div>
+
+          <div className="row">
+            <div className="col-12 col-md-6 col-lg-3 mb-5">
+              <img className="d-block mx-auto img-fluid" src={munifa} alt="awareness icon" height="200px " />
+              <h4 className="text-center">Munifa Ahmad</h4>
+              <h5 className="text-center">Product Owner</h5>
+            </div>
+
+            <div className="col-12 col-md-6 col-lg-3 mb-5">
+              <img className="d-block mx-auto img-fluid" src={wooj} alt="calculate icon" height="200px " />
+              <h4 className="text-center">Woo Jae Chung</h4>
+              <h5 className="text-center">Full-Stack Web Developer</h5>
+            </div>
+
+            <div className="col-12 col-md-6 col-lg-3 mb-5">
+              <img className="d-block mx-auto img-fluid" src={roman} alt="community icon" height="200px "/>
+              <h4 className="text-center">Roman Iudin</h4>
+              <h5 className="text-center">Web Developer</h5>
+            </div>
+
+            <div className="col-12 col-md-6 col-lg-3 mb-5">
+              <img className="d-block mx-auto img-fluid" src={kary} alt="community icon" height="200px "/>
+              <h4 className="text-center">Kary Ma</h4>
+              <h5 className="text-center">Front-End Web Developer</h5>
             </div>
           </div>
         </div>
