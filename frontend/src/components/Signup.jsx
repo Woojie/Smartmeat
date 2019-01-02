22c7d8a64dbf61dc6d6dd20c01a169b46d6c9d67
Kary Ma <karyma101@hotmail.com>
2019-01-01 21:58:07 -0800

    fixed login and sign up

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -9,9 +9,9 @@ const LoginForm = () => {
 
   return(
     <Fragment>
-      <div className="main-body">
-        <form className="d-flex flex-column align-items-center justify-content-center h-100">
-          <p className="text-center h4">Sign Up</p>
+      <div className="main-body light pt-5">
+        <form className="d-flex flex-column align-items-center justify-content-center pt-5">
+          <p className="text-center h4 pt-5">Sign Up</p>
           <div className="form-group">
             <input className="form-control" type="email" onChange={(e)=>getEmail(e.target.value)} placeholder="Enter your email" />
           </div>
