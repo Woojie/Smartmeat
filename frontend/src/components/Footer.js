c020978f0678e923c333d8f3beb5a3c2d6a25de4
romaniudin <romanjudin87@gmail.com>
2019-01-02 23:12:31 -0800

    footer size decreased from Wide to Narrow

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-      <footer className="py-5 bg-dark">
+      <footer className="py-3 bg-dark">
         <div className="text-center primary">&copy;2018 Copyright</div>
       </footer>
     )
