10c08df63099257cb3303a9b04b8bad6ad5bb9da
romaniudin <romanjudin87@gmail.com>
2019-01-08 21:45:18 -0800

    decrease Footer size

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-      <footer className="py-5">
+      <footer className="py-3">
         <div className="text-center">&copy;2018 Copyright</div>
       </footer>
     )
