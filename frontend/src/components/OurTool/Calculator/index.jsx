ba31511e424d17ab0a359643dda3e88fa770d494
romaniudin <romanjudin87@gmail.com>
2019-01-08 21:48:15 -0800

    Fixed responsivness of Calculator page on small screens

--- frontend/src/components/OurTool/Calculator/index.jsx
+++ frontend/src/components/OurTool/Calculator/index.jsx
@@ -6,7 +6,7 @@ import CalcForm from './CalcForm'
 const Calculator = (props) => {
   return(
       <div className="py-5 d-flex flex-column align-items-center">
-        <p className="pt-5 h1 d-block mx-auto">Instant Calculator</p>
+        <p className="pt-5 h1 d-block mx-auto text-center">Instant Calculator</p>
         <CalcForm getCalculation = {props.getCalculation} />
       </div>
   )
